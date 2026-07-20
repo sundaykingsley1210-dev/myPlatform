6512d3f59604261b4e92f204b6433bebb106fa14
sundaykingsley1210 <sundaykingsley1210@gmail.com>
2026-07-20 17:56:58 +0100

    fix: add missing tab handlers (withdraw, new-invest), no-op service worker, CSS cache bust

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,9 +1,7 @@
-const CACHE_NAME = 'enrichu-v13';
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(k => Promise.all(k.map(n => caches.delete(n)))).then(() => self.clients.claim()));
-});
-self.addEventListener('fetch', e => {
-  if (e.request.method !== 'GET') return;
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+  e.waitUntil(
+    caches.keys().then(k => Promise.all(k.map(n => caches.delete(n))))
+    .then(() => self.clients.claim())
+  );
 });
