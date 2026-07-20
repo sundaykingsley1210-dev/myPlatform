1dfeafbb421f732f7da779aaf38fc7ef44426571
sundaykingsley1210 <sundaykingsley1210@gmail.com>
2026-07-20 18:07:10 +0100

    fix: unregister all service workers and clear all caches on every page load

--- public/service-worker.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(k => Promise.all(k.map(n => caches.delete(n))))
-    .then(() => self.clients.claim())
-  );
-});
